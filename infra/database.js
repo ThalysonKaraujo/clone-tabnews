3faf605295b1d0261d1a3f2b540934d6925e8156
Thalyson Kauan <thalyson.kauan7@gmail.com>
2025-10-12 20:33:37 -0300

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -9,11 +9,45 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  client.end();
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.end();
+  }
+  return;
+}
+
+async function version() {
+  const versionQuery = await query({
+    text: "SELECT current_setting('server_version') as version;",
+  });
+  const result = versionQuery.rows[0].version;
+  return result;
+}
+
+async function maxConnections() {
+  const connectionsQuery = await query({
+    text: "SELECT current_setting('max_connections')::float as max_connections;",
+  });
+  const result = connectionsQuery.rows[0].max_connections;
+  return result;
+}
+
+async function openedConnections() {
+  const dbName = "local_db";
+  const connectionsQuery = await query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${dbName}';`,
+  });
+  const result = connectionsQuery.rows[0].count;
   return result;
 }
 
 export default {
   query: query,
+  version: version,
+  maxConnections: maxConnections,
+  openedConnections: openedConnections,
 };
