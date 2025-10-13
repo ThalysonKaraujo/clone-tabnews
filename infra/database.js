02e8098dd6c9ad0a5117588c2d842309bcf90fe6
Thalyson Kauan <thalyson.kauan7@gmail.com>
2025-10-13 20:51:58 -0300

    adds status endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,16 +1,9 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (err) {
@@ -46,9 +39,24 @@ async function openedConnections() {
   return result;
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "production" ? true : false,
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
   query: query,
   version: version,
   maxConnections: maxConnections,
   openedConnections: openedConnections,
+  getNewClient: getNewClient,
 };
