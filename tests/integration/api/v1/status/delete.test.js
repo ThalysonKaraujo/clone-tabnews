aea17a5ef39cb2754d78ea374c05c7a01bf6c196
Thalyson Kauan <thalyson.kauan7@gmail.com>
2025-10-15 22:29:59 -0300

    Adicionando teste do delete

---
+++ tests/integration/api/v1/status/delete.test.js
@@ -0,0 +1,15 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("DROP schema public cascade; create schema public;");
+
+  await database.query("CREATE TABLE posts(id SERIAL PRIMARY KEY);");
+}
+
+test("DELETE to /api/v1/migrations should return 204", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status", {
+    method: "DELETE",
+  });
+});
