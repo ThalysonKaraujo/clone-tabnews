edf7d79f1aa8a5f5432c5f2400ccfefce4191790
Thalyson Kauan <thalyson.kauan7@gmail.com>
2025-10-15 22:36:27 -0300

    Ajustando testes

--- tests/integration/api/v1/migrations/delete.test.js
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -9,7 +9,7 @@ async function cleanDatabase() {
 }
 
 test("DELETE to /api/v1/migrations should return 204", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status", {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "DELETE",
   });
   expect(response.status).toBe(204);
