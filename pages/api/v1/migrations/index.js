34d4281c67d978138f230ec97f83c0ab44273576
Thalyson Kauan <thalyson.kauan7@gmail.com>
2025-10-15 22:44:35 -0300

    Correção concluida

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,6 +1,7 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database.js";
+import { error } from "node:console";
 
 export default async function migrations(req, res) {
   const dbClient = await database.getNewClient();
@@ -13,26 +14,32 @@ export default async function migrations(req, res) {
     migrationsTable: "pgmigrations",
   };
 
-  if (req.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-    await dbClient.end();
-    return res.status(200).json(pendingMigrations);
-  }
-
-  if (req.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
-    if (migratedMigrations.length > 0) {
-      return res.status(200).json(migratedMigrations);
+  try {
+    if (req.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+      return res.status(200).json(pendingMigrations);
     }
-    return res.status(201).json(migratedMigrations);
+
+    if (req.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
+      await dbClient.end();
+      if (migratedMigrations.length > 0) {
+        return res.status(200).json(migratedMigrations);
+      }
+      return res.status(201).json(migratedMigrations);
+    }
+
+    if (req.method === "DELETE") {
+      return res.status(204).json();
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await dbClient.end();
   }
 
-  if (req.method === "DELETE") {
-    return res.status(204).json({ chave: "valor" });
-  }
   res.status(405).end();
 }
