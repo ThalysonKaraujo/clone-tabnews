ce486ae67ac9a923ecf2cdea0b4c47df54853a04
Thalyson Kauan <thalyson.kauan7@gmail.com>
2025-10-17 19:47:09 -0300

    makes npm test stable

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,10 @@
 import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("DROP schema public cascade; create schema public");
-}
+});
 
 test("POST to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
@@ -13,13 +13,6 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(response1.status).toBe(200);
 
   const response1Body = await response1.json();
-  // const names = ["path", "name", "timestamp"];
-  // const keys = Object.keys(responseBody[0]);
-
-  // for (let i = 0; i < 2; i++) {
-  //   expect(keys[i]).toBeDefined();
-  //   expect(keys[i]).toBe(names[i]);
-  // }
 
   expect(Array.isArray(response1Body)).toBe(true);
   expect(response1Body.length).toBeGreaterThan(0);
