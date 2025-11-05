dd528b005c45e65742b4fbe329ee72b6ce70230f
Thalyson Kauan <thalyson.kauan7@gmail.com>
2025-11-05 19:56:01 -0300

    feat: create and use InternalServerError in /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,27 +1,36 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
 
 async function status(req, res) {
-  const updatedAt = new Date().toISOString();
-  const versionResult = await database.version();
-  const maxConnections = await database.maxConnections();
-  const databaseName = process.env.POSTGRES_DB;
+  try {
+    const updatedAt = new Date().toISOString();
+    const versionResult = await database.version();
+    const maxConnections = await database.maxConnections();
+    const databaseName = process.env.POSTGRES_DB;
 
-  const connectionsQuery = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
-    values: [databaseName],
-  });
-  const openedConnections = connectionsQuery.rows[0].count;
+    const connectionsQuery = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+      values: [databaseName],
+    });
+    const openedConnections = connectionsQuery.rows[0].count;
 
-  res.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: versionResult,
-        max_connections: maxConnections,
-        opened_connections: openedConnections,
+    res.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: versionResult,
+          max_connections: maxConnections,
+          opened_connections: openedConnections,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+    console.error(publicErrorObject);
+    res.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
